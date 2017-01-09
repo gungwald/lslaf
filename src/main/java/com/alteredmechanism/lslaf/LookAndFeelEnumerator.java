f3e8414621a4f9a39de0c8c1114a96ace0ef17e8
Bill Chatfield <bill_chatfield@yahoo.com>
2017-01-08 20:53:41 -0500

    Converted from org.dashnine.sleep to Javer

--- src/main/java/com/alteredmechanism/lslaf/LookAndFeelEnumerator.java
+++ src/main/java/com/alteredmechanism/lslaf/LookAndFeelEnumerator.java
@@ -1,32 +1,52 @@
+package com.alteredmechanism.lslaf;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.UIManager;
- 
-$systemLaf = [UIManager getSystemLookAndFeelClassName];
-@lafInfos = [UIManager getInstalledLookAndFeels];
-$lengthOfLongestName = 0;
-$lengthOfLongestClass = 0;
+import javax.swing.UIManager.LookAndFeelInfo;
 
-foreach $info (@lafInfos) {
-    $class = [$info getClassName];
-    $name = [$info getName];
-    %lafs[$name] = $class;
-    $length = strlen($name);
-    if ($length > $lengthOfLongestName) {
-        $lengthOfLongestName = $length;
-    }
-    $classLength = strlen($class);
-    if ($classLength > $lengthOfLongestClass) {
-        $lengthOfLongestClass = $classLength;
-    }
-}
- 
-$lineFormat = "%-$lengthOfLongestName" . "s %-$lengthOfLongestClass" . "s %s";
-foreach $name => $class (%lafs) {
-    [[System out] printf: $lineFormat, @($name, $class)];
-    if ($class eq $systemLaf) {
-        println(" SystemLookAndFeel");
-    }
-    else {
-        println('');
-    }
-}
+public class LookAndFeelEnumerator {
 
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").startsWith("Mac")) {
+            // Stop Java coffee cup icon from appearing in the dock on MacOS.
+            // Older versions of MacOS X require this:
+            System.setProperty("java.awt.headless", "true");
+            // Newer versions (also?) require this:
+            System.setProperty("apple.awt.UIElement", "true");
+        }
+
+        String systemLaf = UIManager.getSystemLookAndFeelClassName();
+        LookAndFeelInfo[] lafInfos = UIManager.getInstalledLookAndFeels();
+        int lengthOfLongestName = 0;
+        int lengthOfLongestClass = 0;
+        Map<String,String> lafs = new HashMap<String,String>();
+        
+        for (LookAndFeelInfo info : lafInfos) {
+            String className = info.getClassName();
+            String name = info.getName();
+            lafs.put(name, className);
+            if (name.length() > lengthOfLongestName) {
+                lengthOfLongestName = name.length();
+            }
+            if (className.length() > lengthOfLongestClass) {
+                lengthOfLongestClass = className.length();
+            }
+        }
+         
+        final String lineFormat = "%-" + lengthOfLongestName + "s %-" + lengthOfLongestClass + "s";
+        for (String name : lafs.keySet()) {
+            String className = lafs.get(name);
+            System.out.printf(lineFormat, name, className);
+            if (className.equals(systemLaf)) {
+                System.out.println(" SystemLookAndFeel");
+            }
+            else {
+                System.out.println();
+            }
+        }
+    
+    }
+
+}
